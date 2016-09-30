bfd10a838c4e19870d68ed6c90d44d0d7abd7023
Samatki <samatki@gmail.com>
2016-09-30 17:38:29 +0100

    Add files via upload

---
+++ pictureLA.js
@@ -0,0 +1,66 @@
+var pictureLinkArray = [
+{
+noPictures : 2,
+pictureLabel : 'Storm'
+},
+
+{
+noPictures : 3,
+pictureLabel : 'Drizzle'
+},
+
+{
+noPictures : 5,
+pictureLabel : 'Rain'
+},
+
+{
+noPictures : 4,
+pictureLabel : 'Snow'
+},
+
+[
+"Mist",
+"Smoke1",
+'Mist',
+'Sand1',
+'Fog1',
+'Sand1',
+'Sand1',
+'Volcano1',
+'Wind1',
+'Tornado'
+],
+
+
+{
+noPictures : 1,
+pictureLabel : 'Clear'
+},
+
+{
+noPictures : 8,
+pictureLabel : 'Cloudy'
+},
+
+{
+noPictures : 3,
+pictureLabel : 'Wind'
+},
+
+{
+noPictures : 1,
+pictureLabel : 'Sstorm'
+},
+
+[
+'Tornado',
+'Sstorm1',
+'Sstorm1',
+'Snow1',
+'Hot',
+'Wind1',
+'Hail1'
+]
+
+]
